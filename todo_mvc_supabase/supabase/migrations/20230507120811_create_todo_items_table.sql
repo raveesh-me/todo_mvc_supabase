4eaff84ca08c7abd4fdcb26a83f9aee54a0cdfda
Raveesh Agarwal <raveeshr503@gmail.com>
2023-05-09 01:07:28 +0530

    remove: older supabase deployment

--- todo_mvc_supabase/supabase/migrations/20230507120811_create_todo_items_table.sql
+++
@@ -1,9 +0,0 @@
-
-create table
-TodoItems (
-id bigint primary key generated always as identity,
-title  text,
-description text,
-is_completed boolean default false,
-created_at timestamptz default now()
-);
--- todo_mvc_supabase/supabase/seed.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public.todo_items (title, description, is_completed, created_at, complete_by)
-SELECT
-  CONCAT('Task ', num),
-  CONCAT('Description for task ', num),
-  CASE WHEN num % 2 = 0 THEN true ELSE false END,
-  NOW() - (INTERVAL '1 day' * (500 - num)),
-  NOW() + (INTERVAL '1 day' * (num % 30))
-FROM generate_series(1, 200) num;
